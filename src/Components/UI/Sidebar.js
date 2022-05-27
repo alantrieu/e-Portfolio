60620bd9f94df8a6ed71c56f82f2c6a1109a7abc
Alan932 <47587128+Alan932@users.noreply.github.com>
2022-05-27 22:36:13 +1000

    added a Sidebar

---
+++ src/Components/UI/Sidebar.js
@@ -0,0 +1,71 @@
+import './Sidebar.scss';
+
+import { Link, NavLink } from 'react-router-dom';
+import MyLogo from '../../assets/images/my-logo.png';
+import MyLogoSub from '../../assets/images/my-logo-sub.png';
+import MyLinks from '../../assets/images/my-links.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => {
+    return (
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={MyLogo} alt="logo" />
+                <img className="sub-logo" src={MyLogoSub} alt="Alan" />
+            </Link>
+            
+            <nav>
+                <NavLink 
+                    exact="true"
+                    activeclassname="activate" 
+                    to="/"
+                >
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink 
+                    exact="true" 
+                    activeclassname="activate" 
+                    className="about-link" 
+                    to="/about"
+                >
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink 
+                    exact="true" 
+                    activeclassname="activate" 
+                    className="contact-link" 
+                    to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+
+            
+            <ul>
+                <img src={MyLinks} alt="my links" />
+                <li>
+                    <a 
+                        target="_blank" 
+                        rel='noreferrer' 
+                        href='https://www.linkedin.com/in/alan-trieu-165837180/'
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        target="_blank"
+                        rel='noreferrer'
+                        href='https://github.com/alantrieu'
+                    >
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    );
+};
+
+export default Sidebar;
