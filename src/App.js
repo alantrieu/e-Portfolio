f91f3c82c3badf7ec8135b60c06643f5f6b285ec
Alan932 <47587128+Alan932@users.noreply.github.com>
2022-05-28 00:10:36 +1000

    added a homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-// import Homepage from './Components/Homepage/Homepage';
 import './App.css';
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
+import Homepage from './Components/Homepage/Homepage';
 
 function App() {
-//   return (
-//     <div className="App">
-//         <Homepage />
-//     </div>
-//   );
-
     return (
         <>
             <Routes>
-                <Route path="/" element={<Layout />} />
+                <Route path="/" element={<Layout />} >
+                    <Route index element={<Homepage />} />
+                </Route>
             </Routes>
         </>
     );
