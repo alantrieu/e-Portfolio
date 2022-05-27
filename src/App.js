a7ed8764c5ad6b89e8e48e42488771a4a7bb5ab0
Alan932 <47587128+Alan932@users.noreply.github.com>
2022-05-27 22:33:04 +1000

    added starting files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// import Homepage from './Components/Homepage/Homepage';
 import './App.css';
 
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout/Layout';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//         <Homepage />
+//     </div>
+//   );
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />} />
+            </Routes>
+        </>
+    );
+};
 
 export default App;
